3f73aa3c7bd94a532301141b890c599bd080fbce
Andrew Hong <adrw.hong@gmail.com>
2015-02-27 12:57:59 -0500

    Initial commit of our new baby

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# synergyspace2
-django love
+
+
+# synergyspace
+
+synergyspace is a (short description) built with [Python][0] using the [Django Web Framework][1].
+
+This project has the following basic apps:
+
+* App1 (short desc)
+* App2 (short desc)
+* App3 (short desc)
+
+## Installation
+
+### Quick start
+
+To set up a development environment quickly, first install Python 3.4. It
+comes with virtualenv built-in. So create a virtual env by:
+
+    1. `$ python3.4 -m venv synergyspace`
+    2. `$ . synergyspace/bin/activate`
+
+> Sometimes, binaries like pip get installed inside `local/bin/`. So append
+> this line to `synergyspace/bin/activate`:
+>
+> `PATH="$VIRTUAL_ENV/local/bin:$PATH"`
+
+Now the pip commands should work smoothly. Install all dependencies:
+
+    pip install -r dev-requirements.txt
+
+Run migrations:
+
+    python manage.py migrate
+
+### Detailed instructions
+
+Take a look at the docs for a detailed instructions guide.
+
+[0]: https://www.python.org/
+[1]: https://www.djangoproject.com/
---
+++ src/accounts/views.py
@@ -0,0 +1,69 @@
+from __future__ import unicode_literals
+from django.core.urlresolvers import reverse_lazy
+from django.views import generic
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.contrib import messages
+from authtools import views as authviews
+from braces import views as bracesviews
+from . import forms
+
+User = get_user_model()
+
+
+class LoginView(bracesviews.AnonymousRequiredMixin,
+                authviews.LoginView):
+    template_name = "accounts/login.html"
+    form_class = forms.LoginForm
+
+
+class LogoutView(authviews.LogoutView):
+    url = reverse_lazy('home')
+
+
+class SignUpView(bracesviews.AnonymousRequiredMixin,
+                 bracesviews.FormValidMessageMixin,
+                 generic.CreateView):
+    form_class = forms.SignupForm
+    model = User
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('home')
+    form_valid_message = "You're signed up!"
+
+    def form_valid(self, form):
+        r = super(SignUpView, self).form_valid(form)
+        username = form.cleaned_data["email"]
+        password = form.cleaned_data["password1"]
+        user = auth.authenticate(email=username, password=password)
+        auth.login(self.request, user)
+        return r
+
+
+class PasswordChangeView(authviews.PasswordChangeView):
+    form_class = forms.PasswordChangeForm
+    template_name = 'accounts/password-change.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request,
+                         "Your password was changed, "
+                         "hence you have been logged out. Please relogin")
+        return super(PasswordChangeView, self).form_valid(form)
+
+
+class PasswordResetView(authviews.PasswordResetView):
+    form_class = forms.PasswordResetForm
+    template_name = 'accounts/password-reset.html'
+    success_url = reverse_lazy('accounts:password-reset-done')
+    subject_template_name = 'accounts/emails/password-reset-subject.txt'
+    email_template_name = 'accounts/emails/password-reset-email.html'
+
+
+class PasswordResetDoneView(authviews.PasswordResetDoneView):
+    template_name = 'accounts/password-reset-done.html'
+
+
+class PasswordResetConfirmView(authviews.PasswordResetConfirmAndLoginView):
+    template_name = 'accounts/password-reset-confirm.html'
+    form_class = forms.SetPasswordForm
